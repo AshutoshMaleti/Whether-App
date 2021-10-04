f595d3be399aef9ee6e461a4e1bf1a6cc371583b
AshutoshMaleti <ashutosh.maleti@dianapps.com>
2021-10-04 09:45:22 +0530

    random commit

--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db.models.base import Model
 
 # Create your models here.
 class City(models.Model):
-    city = models.CharField(max_length=25)
+    city = models.CharField(max_length=25, verbose_name='city')
 
     class Meta:
         db_table = 'City'
